73ac20b9912ddf7ef5e1cae777f1ff2208d02633
Kerem Gurel <keremgurell99@gmail.com>
2020-10-02 21:54:50 -0700

    Added logo image

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,14 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 import "./Header.css";
+import logo from "../src/Images/logo.png";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 
 function Header() {
   return (
     <div className="header">
-      <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" />
+      <img className="header__logo" src={logo} alt="" />
 
       <div className="header__search">
         <input className="header__searchInput" type="text" />
@@ -17,7 +18,7 @@ function Header() {
 
       <div className="header__nav">
         <div className="header__option">
-          <span className="header__optionLineOne">Hello Guest</span>
+          <span className="header__optionLineOne">Hello!</span>
           <span classNam="header__optionLineTwo">Sign In</span>
         </div>
 
