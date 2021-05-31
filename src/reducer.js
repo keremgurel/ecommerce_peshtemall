df3f5d4bfcb13355f22f16379f48d15d7303c4ff
Kerem Gurel <keremgurell99@gmail.com>
2021-05-30 23:00:33 -0700

    fix: user auth functionality

--- src/reducer.js
+++ src/reducer.js
@@ -31,6 +31,12 @@ const reducer = (state, action) => {
             }
 
             return {...state, basket: newBasket}
+
+            case "SET_USER":
+                return {
+                    ...state,
+                    user: action.user
+                }
         
         default:
             return state;
