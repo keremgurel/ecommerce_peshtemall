30369846eef1757be6b8d56a0231ae1f0b20027b
Kerem Gurel <keremgurell99@gmail.com>
2021-06-01 21:40:42 -0700

    feat: backend deployed

---
+++ functions/index.js
@@ -0,0 +1,39 @@
+// @ts-nocheck
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+// eslint-disable-next-line max-len
+const stripe = require("stripe")("sk_test_51IxhACEL2O8em9MWBUV5SyJvbDgFKXibph6s7P1FXwROADLkmY8CLfFYqWkgBteUTVDKfo9TFUMGbaKt5G8JRUTQ00ZpaQU9AB"
+);
+
+// API
+
+// - App config
+const app = express();
+
+// - Middlewares
+app.use(cors({origin: true}));
+app.use(express.json());
+
+// - API routes
+app.get("/", (request, response) => response.status(200).send("hello world"));
+
+app.post("/payments/create", async (request, response) => {
+  const total = request.query.total;
+
+  console.log("Payment Request Recieved BOOM!!! for this amount >>> ", total);
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total, // subunits of the currency
+    currency: "usd",
+  });
+
+  // OK - Created
+  response.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+
+// - Listen command
+exports.api = functions.https.onRequest(app);
